dd188a0ab5164789fe12fb48a3201ae112f887b8
Drillibit <293344asd@gmail.com>
2017-09-10 17:35:06 +0600

    some testingwith react

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+require('./style/css/main.sass');
 const App = () => {
-  return <div>Hi!</div>;
+  return <div>Hello world!</div>;
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
